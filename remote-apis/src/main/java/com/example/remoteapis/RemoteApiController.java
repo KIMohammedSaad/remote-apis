fd8bf84aadfe4aef0a8f9ae72fd7d225f0237e7b
Saad <mohammedsaad7866@gmail.com>
2023-07-10 23:18:03 +0530

    Adding Remote Api Project

---
+++ remote-apis/src/main/java/com/example/remoteapis/RemoteApiController.java
@@ -0,0 +1,10 @@
+package com.example.remoteapis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remote")
+public class RemoteApiController {
+
+}
